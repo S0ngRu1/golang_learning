96688a4f7acc8694aba2836814dd5eb44244975e
S0ngRu1 <1922909737@qq.com>
2025-12-13 23:49:40 +0800

    feat :add interface

--- 11-oop/test_oop.go
+++ 11-oop/test_oop.go
@@ -27,11 +27,56 @@ func (this *Hero) SetName(newName string){
 	// 这个this是调用方法对象的地址
 	this.Name = newName 
 } 
-func main() {
-	// 创建一个对象
-	hero := Hero{Name: "zhang3",Ad: 100, Level: 1}
-	hero.Show()
-	hero.GetName()
-	hero.SetName("李四")
-	hero.Show()
+
+type Human struct{
+	Name string
+	Sex string
+}
+
+func (this *Human) Eat(){
+	fmt.Println("Human.Eat()...")
+}
+
+func (this *Human) Walk(){
+	fmt.Println("Human.Walk()...")
+}
+
+
+
+// 继承
+type SuperMan struct{
+	Human  // SuperMan 类继承了Human类的方法
+
+	level int
+}
+func (this *SuperMan) Eat(){
+	fmt.Println("SuperMan.Eat()")
+}
+
+func (this *SuperMan) Fly(){
+	fmt.Println("SuperMan.Fly()")
+}
+
+
+
+func test_class() {
+	// // 创建一个对象
+	// hero := Hero{Name: "zhang3",Ad: 100, Level: 1}
+	// hero.Show()
+	// hero.GetName()
+	// hero.SetName("李四")
+	// hero.Show()
+
+	zhangsan := Human{"zhangsan", "female"}
+
+	zhangsan.Eat()
+	zhangsan.Walk()
+
+	// 定义一个子类对象
+	// 子类名{父类名{父类属性}，子类属性}
+	s := SuperMan{Human{"li","female"},88}
+	s.Walk()
+	s.Eat()
+	s.Fly()
+
 }
